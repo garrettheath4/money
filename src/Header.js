05c29bb8a16f90e63f2b8f15ae0ccaca54531b48
Garrett Koller <gkoller@catalist.us>
2018-01-24 13:34:35 -0500

    Added Bootstrap 4.0.0 and created a Header component with a jumbotron for the main page

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div class="jumbotron">
+        <h1>Garrett Koller</h1>
+        <p>Software Engineer in Washington, DC. BS in Computer Science from Washington and Lee University. Originally from Oklahoma.</p>
+      </div>
+    );
+  }
+}
+
+export default Header;

5dd50d99dbf012b8303f7a3cc9ea543de85ed882
Garrett Koller <gkoller@catalist.us>
2018-01-29 22:38:18 -0500

    MoneyApp Component now acts as a template/container based on its title and priority properties

--- src/MoneyApps/MoneyApp.jsx
+++ src/MoneyApps/MoneyApp.jsx
@@ -2,12 +2,67 @@ import React, { Component } from 'react';
 import './MoneyApp.css';
 
 class MoneyApp extends Component {
+  constructor(props) {
+    super(props);
+    var priority = 'medium';
+    if ('priority' in props && isValidPriority(props.priority)) {
+      priority = toValidPriority(props.priority);
+    }
+    this.state = {
+      priority: priority,
+    };
+  }
+
   render() {
     return (
-      <div className="card MoneyApp">
+      <div className={priority2bsCardClass(this.state.priority)}>
+        <div className="card-header">
+          <h3 className="card-title">{this.props.title}</h3>
+        </div>
+        <div className="card-block">
+          {this.props.children}
+        </div>
       </div>
     );
   }
+}
+
+function arrayContains(ary, item) {
+  return ary.indexOf(item) > -1;
+}
+
+function isValidPriority(priority) {
+  const priorityClean = priority.trim().toLowerCase();
+  return arrayContains(['low', 'medium', 'high'], priorityClean);
+}
+
+function toValidPriority(priority) {
+  const priorityClean = priority.trim().toLowerCase();
+  if (isValidPriority(priorityClean)) {
+    return priorityClean;
+  } else {
+    return 'medium';
+  }
+}
+
+function priority2bsColor(priority) {
+  if (!isValidPriority(priority)) {
+    return 'info';
+  }
+  const priorityValid = toValidPriority(priority);
+  switch (priorityValid) {
+    case 'high':
+      return 'primary';
+    case 'low':
+      return 'muted';
+    case 'medium':
+    default:
+      return 'info';
+  }
+}
+
+function priority2bsCardClass(priority) {
+  return "card card-" + priority2bsColor(priority) + " MoneyApp";
 }
 
 export default MoneyApp;
--- src/MoneyApps/SquareWeb.jsx
+++ src/MoneyApps/SquareWeb.jsx
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
-import './MoneyApp.css';
+import MoneyApp from './MoneyApp'
 
 class SquareWeb extends Component {
   render() {
     return (
-      <div className="card MoneyApp">
-        <div className="card-header">
-          <h3 className="card-title">Cash Website</h3>
-        </div>
-        <div className="card-block">
-          <h4>If you don't want to sign up for a cash app, use this webpage to send me money using your debit card. Payments are sent <a href="https://cash.me/security">securely</a> by Square Cash.</h4>
-          <a href="https://cash.me/$garrettheath4" className="btn btn-secondary cashtag-link" role="button">
-            <span className="dollars-unknown show">Send me money with the Square Cash website</span>
-            <span className="dollars-known hide" hidden>Send me $<span className="dollars-known-amount"></span> with the Square Cash website</span>
-          </a>
-        </div>
-      </div>
+      <MoneyApp title="Square Cash Website" priority="low">
+        <h4>If you don't want to sign up for a cash app, use this webpage to send me money using your debit card. Payments are sent <a href="https://cash.me/security">securely</a> by Square Cash.</h4>
+        <a href="https://cash.me/$garrettheath4" className="btn btn-secondary cashtag-link" role="button">
+          <span className="dollars-unknown show">Send me money with the Square Cash website</span>
+          <span className="dollars-known hide" hidden>Send me $<span className="dollars-known-amount"></span> with the Square Cash website</span>
+        </a>
+      </MoneyApp>
     );
   }
 }
